c08a6df12a351807fbdf0a4fef3dc276c81e34ed
reaper <ahoy@barelyhuman.dev>
2023-04-24 18:54:34 +0530

    polish

--- src/components/TimelineItem.island.js
+++ src/components/TimelineItem.island.js
@@ -11,19 +11,19 @@ export default function TimelineItem({ name, time, children }) {
         <div
           ref={node => {
             const box = node.parentNode.nextSibling.getBoundingClientRect()
-            const toAdd = node.getBoundingClientRect().y - box.y
+            const toAdd = Math.abs(node.getBoundingClientRect().y - box.y)
             node.style.height = toAdd + 'px'
           }}
-          class="h-4 w-[1px] bg-zinc-200"
+          class="h-4 w-[1px] bg-zinc-100"
         ></div>
-        <div class="relative h-4 w-4 flex-none rounded-full bg-black drop-shadow-2xl">
-          <p class="text-zinc-400 absolute right-7 top-[50%] translate-y-[-50%]">
+        <div class="relative h-4 w-4 flex-none rounded-full bg-zinc-700 drop-shadow-xl">
+          <p class="text-zinc-400 absolute text-xs right-7 top-[50%] translate-y-[-50%]">
             {time}
           </p>
         </div>
-        <div class="h-full flex-1 w-[1px] bg-zinc-200"></div>
+        <div class="h-full flex-1 w-[1px] bg-zinc-100"></div>
       </div>
-      <div class="ml-10 py-6 flex flex-col w-full">
+      <div class="ml-10  flex flex-col w-full">
         <div>
           <h1 class="font-bold text-zinc-700">{name}</h1>
         </div>
